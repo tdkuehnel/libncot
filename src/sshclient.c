ce2f2c7fa60a2b957d162cb47e54519dd894a126
Torsten Kuehnel <tdkuehnel@naturfreundeskreis.de>
2019-12-24 22:31:51 +0100

    Finally got a working auth NONE libssh connection sample (5 bytes transferred).

--- src/sshclient.c
+++ src/sshclient.c
@@ -4,6 +4,225 @@
 #include <errno.h>
 #include <string.h>
 
+/** Zero a structure */
+#define ZERO_STRUCT(x) memset((char *)&(x), 0, sizeof(x))
+
+int authenticate_kbdint(ssh_session session, const char *password)
+{
+    int err;
+
+    err = ssh_userauth_kbdint(session, NULL, NULL);
+    while (err == SSH_AUTH_INFO) {
+        const char *instruction;
+        const char *name;
+        char buffer[128];
+        int i, n;
+
+        name = ssh_userauth_kbdint_getname(session);
+        instruction = ssh_userauth_kbdint_getinstruction(session);
+        n = ssh_userauth_kbdint_getnprompts(session);
+
+        if (name && strlen(name) > 0) {
+            printf("%s\n", name);
+        }
+
+        if (instruction && strlen(instruction) > 0) {
+            printf("%s\n", instruction);
+        }
+
+        for (i = 0; i < n; i++) {
+            const char *answer;
+            const char *prompt;
+            char echo;
+
+            prompt = ssh_userauth_kbdint_getprompt(session, i, &echo);
+            if (prompt == NULL) {
+                break;
+            }
+
+            if (echo) {
+                char *p;
+
+                printf("%s", prompt);
+
+                if (fgets(buffer, sizeof(buffer), stdin) == NULL) {
+                    return SSH_AUTH_ERROR;
+                }
+
+                buffer[sizeof(buffer) - 1] = '\0';
+                if ((p = strchr(buffer, '\n'))) {
+                    *p = '\0';
+                }
+
+                if (ssh_userauth_kbdint_setanswer(session, i, buffer) < 0) {
+                    return SSH_AUTH_ERROR;
+                }
+
+                memset(buffer, 0, strlen(buffer));
+            } else {
+                if (password && strstr(prompt, "Password:")) {
+                    answer = password;
+                } else {
+                    buffer[0] = '\0';
+
+                    if (ssh_getpass(prompt, buffer, sizeof(buffer), 0, 0) < 0) {
+                        return SSH_AUTH_ERROR;
+                    }
+                    answer = buffer;
+                }
+                err = ssh_userauth_kbdint_setanswer(session, i, answer);
+                memset(buffer, 0, sizeof(buffer));
+                if (err < 0) {
+                    return SSH_AUTH_ERROR;
+                }
+            }
+        }
+        err=ssh_userauth_kbdint(session,NULL,NULL);
+    }
+
+    return err;
+}
+
+static int auth_keyfile(ssh_session session, char* keyfile)
+{
+    ssh_key key = NULL;
+    char pubkey[132] = {0}; // +".pub"
+    int rc;
+
+    snprintf(pubkey, sizeof(pubkey), "%s.pub", keyfile);
+
+    rc = ssh_pki_import_pubkey_file( pubkey, &key);
+
+    if (rc != SSH_OK)
+        return SSH_AUTH_DENIED;
+
+    rc = ssh_userauth_try_publickey(session, NULL, key);
+
+    ssh_key_free(key);
+
+    if (rc!=SSH_AUTH_SUCCESS)
+        return SSH_AUTH_DENIED;
+
+    rc = ssh_pki_import_privkey_file(keyfile, NULL, NULL, NULL, &key);
+
+    if (rc != SSH_OK)
+        return SSH_AUTH_DENIED;
+
+    rc = ssh_userauth_publickey(session, NULL, key);
+
+    ssh_key_free(key);
+
+    return rc;
+}
+
+
+static void error(ssh_session session)
+{
+    fprintf(stderr,"Authentication failed: %s\n",ssh_get_error(session));
+}
+
+int authenticate_console(ssh_session session)
+{
+    int rc;
+    int method;
+    char password[128] = {0};
+    char *banner;
+
+    // Try to authenticate
+    rc = ssh_userauth_none(session, NULL);
+    if (rc == SSH_AUTH_ERROR) {
+        error(session);
+        return rc;
+    }
+
+    method = ssh_userauth_list(session, NULL);
+    while (rc != SSH_AUTH_SUCCESS) {
+        if (method & SSH_AUTH_METHOD_GSSAPI_MIC){
+            rc = ssh_userauth_gssapi(session);
+            if(rc == SSH_AUTH_ERROR) {
+                error(session);
+                return rc;
+            } else if (rc == SSH_AUTH_SUCCESS) {
+                break;
+            }
+        }
+        // Try to authenticate with public key first
+        if (method & SSH_AUTH_METHOD_PUBLICKEY) {
+            rc = ssh_userauth_publickey_auto(session, NULL, NULL);
+            if (rc == SSH_AUTH_ERROR) {
+                error(session);
+                return rc;
+            } else if (rc == SSH_AUTH_SUCCESS) {
+                break;
+            }
+        }
+        {
+            char buffer[128] = {0};
+            char *p = NULL;
+
+            printf("Automatic pubkey failed. "
+                   "Do you want to try a specific key? (y/n)\n");
+            if (fgets(buffer, sizeof(buffer), stdin) == NULL) {
+                break;
+            }
+            if ((buffer[0]=='Y') || (buffer[0]=='y')) {
+                printf("private key filename: ");
+
+                if (fgets(buffer, sizeof(buffer), stdin) == NULL) {
+                    return SSH_AUTH_ERROR;
+                }
+
+                buffer[sizeof(buffer) - 1] = '\0';
+                if ((p = strchr(buffer, '\n'))) {
+                    *p = '\0';
+                }
+
+                rc = auth_keyfile(session, buffer);
+
+                if(rc == SSH_AUTH_SUCCESS) {
+                    break;
+                }
+                fprintf(stderr, "failed with key\n");
+            }
+        }
+
+        // Try to authenticate with keyboard interactive";
+        if (method & SSH_AUTH_METHOD_INTERACTIVE) {
+            rc = authenticate_kbdint(session, NULL);
+            if (rc == SSH_AUTH_ERROR) {
+                error(session);
+                return rc;
+            } else if (rc == SSH_AUTH_SUCCESS) {
+                break;
+            }
+        }
+
+        if (ssh_getpass("Password: ", password, sizeof(password), 0, 0) < 0) {
+            return SSH_AUTH_ERROR;
+        }
+
+        // Try to authenticate with password
+        if (method & SSH_AUTH_METHOD_PASSWORD) {
+            rc = ssh_userauth_password(session, NULL, password);
+            if (rc == SSH_AUTH_ERROR) {
+                error(session);
+                return rc;
+            } else if (rc == SSH_AUTH_SUCCESS) {
+                break;
+            }
+        }
+        memset(password, 0, sizeof(password));
+    }
+
+    banner = ssh_get_issue_banner(session);
+    if (banner) {
+        printf("%s\n",banner);
+        SSH_STRING_FREE_CHAR(banner);
+    }
+
+    return rc;
+}
+
 int verify_knownhost(ssh_session session)
 {
     enum ssh_known_hosts_e state;
@@ -81,36 +300,84 @@ int verify_knownhost(ssh_session session)
 
 int main(int argc, char **argv)
 {
-  ssh_session my_ssh_session;
-  int verbosity = SSH_LOG_PROTOCOL;
-  int port = 22;
-  int rc;
+	struct ssh_channel_struct *channel;
+	struct ssh_session_struct *session;
+	int verbosity = SSH_LOG_PACKET;
+	int port = 2244;
+	int rc;
+	ssh_key key = NULL;
+	ssh_key pkey = NULL;
+	const char* buf = "Hallo";
+	session = ssh_new();
+	if (session == NULL)
+		exit(-1);
 
-  my_ssh_session = ssh_new();
-  if (my_ssh_session == NULL)
-    exit(-1);
+	/* ssh_bind_options_set(sshbind, SSH_BIND_OPTIONS_CONFIG_DIR, "./"); */
+	ssh_options_set(session, SSH_OPTIONS_HOST, "localhost");
+	ssh_options_set(session, SSH_OPTIONS_LOG_VERBOSITY, &verbosity);
+	ssh_options_set(session, SSH_OPTIONS_PORT, &port);
 
-  ssh_options_set(my_ssh_session, SSH_OPTIONS_HOST, "localhost");
-  ssh_options_set(my_ssh_session, SSH_OPTIONS_LOG_VERBOSITY, &verbosity);
-  ssh_options_set(my_ssh_session, SSH_OPTIONS_PORT, &port);
+	rc = ssh_connect(session);
+	if (rc != SSH_OK)
+	{
+		fprintf(stderr, "Error connecting to localhost: %s\n",
+			ssh_get_error(session));
+		exit(-1);
+	}
+	printf("Mark 1\n");
 
-  rc = ssh_connect(my_ssh_session);
-  if (rc != SSH_OK)
-  {
-    fprintf(stderr, "Error connecting to localhost: %s\n",
-            ssh_get_error(my_ssh_session));
-    exit(-1);
-  }
+	// Verify the server's identity
+	if (verify_knownhost(session) < 0)
+	{
+		ssh_disconnect(session);
+		ssh_free(session);
+		exit(-1);
+	}
+	printf("Mark 2\n");
 
-  // Verify the server's identity
-  if (verify_knownhost(my_ssh_session) < 0)
-  {
-    ssh_disconnect(my_ssh_session);
-    ssh_free(my_ssh_session);
-    exit(-1);
-  }
+	if (ssh_pki_import_pubkey_file("/home/tdkuehnel/.ssh/id_rsa.pub", &pkey) != SSH_OK) {
+		fprintf(stderr, "Error loading pubkey file \n",
+			ssh_get_error(session));
+		exit(-1);
+	}
+	printf("Mark 3\n");
 
-  ssh_disconnect(my_ssh_session);
-  ssh_free(my_ssh_session);
-  printf("done\n");
+	/* if (ssh_userauth_try_publickey(session, NULL, pkey) != SSH_AUTH_SUCCESS) { */
+	/*   fprintf(stderr, "Error trying to authenticate with pubkey file \n", */
+	/*           ssh_get_error(session)); */
+	/*   exit(-1); */
+	/* } */
+	/* if (ssh_userauth_autopubkey(session, NULL) != SSH_AUTH_SUCCESS) */
+	/* 	  fprintf(stderr, "Error authenticating\n"); */
+
+	/* while (rc = authenticate_kbdint(session, "libssh") != SSH_AUTH_SUCCESS) { */
+	/* 	  fprintf(stderr, "authenticating step: rc: %d\n", rc); */
+	/* } */
+	rc = ssh_userauth_none(session, NULL);
+
+	if (rc == SSH_AUTH_SUCCESS)
+		printf("authentication success\n");
+
+	printf("Mark 4\n");
+
+	channel = ssh_channel_new(session);
+	if (!channel)
+		printf("error creating channel\n");
+	else
+		printf("ssh_channel_new ok\n");
+
+	ssh_channel_open_session(channel);
+
+
+	if (!ssh_channel_write(channel, buf, 5) == 5)
+		printf("error sending 5 bytes over channel\n");
+	else
+		printf("5 bytes send\n");
+
+	sleep(1);
+	ssh_channel_close(channel);
+
+	ssh_disconnect(session);
+	ssh_free(session);
+	printf("done\n");
 }
